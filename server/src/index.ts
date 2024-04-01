6fc16143f0c448bc9e0fab6f5f601748794e4e1f
Reinhard Langner <ReinhardLangner@gmx.de>
2024-04-01 12:19:53 +0200

    add server

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import { startHttpServer } from "./server";
+
+async function main() {
+  console.log("multi-user starting ...");
+
+  startHttpServer();
+}
+
+main();
---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+function startHttpServer() {
+  const express = require("express");
+
+  const app = express();
+
+  app.use(express.json());
+
+  const port = process.env.PORT || 8080; // default port to listen
+
+  app.get("/", (req: Request, res: Response) => {
+    let result = "hello world";
+    res.send(`<code>${result}</code>`);
+  });
+
+  app.listen(port, () => {
+    console.log("server listen to port:", port);
+  });
+}
+
+export { startHttpServer };
